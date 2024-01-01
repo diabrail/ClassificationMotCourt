52e4e0e7a464020dd74f7bbf5c632104421f1071
laminedor <gueye.lamine1@ugb.edu.sn>
2024-01-01 01:47:44 +0000

    css

--- app.py
+++ app.py
@@ -3,13 +3,12 @@ from model_naive_bayes.naive_bayes import TextClassifier
 from representation_donnees.representation import TextRepresentation
 from pretraitement_donnees.pretraitement import DataPreprocessor
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-app = Flask(__name__)
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 classifier = TextClassifier()
 representation = TextRepresentation()
 preprocessor = DataPreprocessor(filename=None)
 tfidf_vectorizer = TfidfVectorizer()
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -18,18 +17,14 @@ def index():
 def classify():
     if request.method == 'POST':
         tweet = request.form['tweet']
-
         # Prétraitement du tweet
         preprocessed_tweet = preprocessor.clean_tweet(tweet)
         preprocessed_tweet = preprocessor.tokenize_text(preprocessed_tweet)
         preprocessed_tweet = preprocessor.lemmatize_text(preprocessed_tweet)
-
         # Représentation du tweet en vecteur TF-IDF
         tweet_tfidf = tfidf_vectorizer.transform([preprocessed_tweet])
-
         # Classification du tweet
         result = classifier.classify(tweet_tfidf)
-
         return render_template('index.html', tweet=tweet, result=result)
 
 if __name__ == '__main__':
